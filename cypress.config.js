fde11e1ad9307ebf5080d620919c7d36cd1c0f3d
Lauren Thorud <lthorud12@gmail.com>
2025-02-16 22:56:12 -0600

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from "cypress";
+import vitePreprocessor from "cypress-vite";
+
+export default defineConfig({
+  reporter: "junit",
+  reporterOptions: {
+    mochaFile: "results/test-[hash].xml",
+  
+  },
+  env: {
+    codeCoverage: {
+      exclude: "cypress/**/*.*"
+    }
+  },
+  e2e: {
+    experimentalStudio: true,
+    baseUrl: 'http://localhost:5173',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config) 
+      return config;
+    }
+  },
+  viewportHeight: 900,
+  viewportWidth: 1440
+});
